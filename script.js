53b6b2a56fa444dfddf9fb85ba898e577e1348be
symsym <sym.playing@gmail.com>
2023-11-13 09:58:17 +0100

    modifs

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ startBtn.addEventListener("click", startGame());
 function startGame(name) {
     sessionStorage.setItem("life", "20");
     sessionStorage.setItem("name", name);
+    sessionStorage.setItem("courage", 0);
 }
+
+function addCourage(num) {
+    sessionStorage.setItem("courage", sessionStorage.getItem("courage") + num);
+}
+
 
