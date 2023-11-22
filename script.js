b5e483888e249a15a7427929a4f06e4bbf7a07d4
Lajoum <amaury.debellabre@gmail.com>
2023-11-22 11:47:36 +0100

    Ajout des infos + nom du perso
    Co-authored-by: symsym <sym.playing@gmail.com>

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 // startBtn = document.querySelector(".startBtn");
 // startBtn.addEventListener("click", startGame());
 
-function startGame(name) {
-    sessionStorage.setItem("life", "20");
-    sessionStorage.setItem("name", name);
-    sessionStorage.setItem("courage", 0);
-}
 
 function addCourage(num) {
-    sessionStorage.setItem("courage", sessionStorage.getItem("courage") + num);
+    localStorage.setItem("courage", localStorage.getItem("courage") + num);
+}
+function addForce(num) {
+    localStorage.setItem("force", localStorage.getItem("force") + num);
+}
+function startGame() {
+    var name = document.getElementById("login").value;
+    localStorage.setItem("name", name);
+    localStorage.setItem("life", 20);
+    localStorage.setItem("courage", 0);
+    localStorage.setItem("force", 5);
+
+}
+function name() {
+    var number = "123";
+    document.getElementById("myText").innerHTML = number;
+}
+
+function barre() {
+    var life = localStorage.getItem("life");
+    var courage = localStorage.getItem("courage");
+    var name = localStorage.getItem("name");
+    document.getElementById("info").innerHTML = "<p> Nom : " + name + "</p><p> Courage : " + courage + "</p><p> Vie : " + life + "</p>";
 }
 
 function transition(issue) {
